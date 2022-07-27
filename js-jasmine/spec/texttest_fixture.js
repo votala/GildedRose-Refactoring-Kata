685a40ff6efe13d2e2d0c8425bd32a40311c80b7
Volkan <whatdadj@msn.com>
2022-07-27 06:23:33 +0200

    Refactoring met JS

--- js-jasmine/spec/texttest_fixture.js
+++ js-jasmine/spec/texttest_fixture.js
@@ -21,7 +21,6 @@ const gildedRose = new Shop(items);
 console.log("OMGHAI!");
 for (let day = 0; day < days; day++) {
   console.log(`\n-------- day ${day} --------`);
-  console.log("name, sellIn, quality");
-  items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
+  console.table(items);
   gildedRose.updateQuality();
 }
--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -6,61 +6,83 @@ class Item {
   }
 }
 
+
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
-  updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
 
+  #itemTypes = {
+    brie : "Aged Brie",
+    backstage : "Backstage passes to a TAFKAL80ETC concert",
+    conjured : "Conjured Mana Cake",
+    sulfuras : "Sulfuras, Hand of Ragnaros",
+    exilir : "Elixir of the Mongoose",
+    vest: "+5 Dexterity Vest",
+  }
+
+  updateQuality() {
+    this.items.forEach(item => {
+      let letSell=true;
+      switch (item.name) {
+        case this.#itemTypes.brie:
+          item.quality = ShopHelper.increaseInt(item.quality);
+          break;
+        case this.#itemTypes.backstage:
+          
+          item.quality = 
+            item.sellIn < 0 ? 0 : // if sellIn < 0, quality = 0
+            ShopHelper.increaseInt(
+              item.quality,
+              item.sellIn > 10 ? 1 : // +1 quality for each day until 10 days left
+              item.sellIn > 5 ? 2 : 3 // +2 quality for each day until 5 days left
+            );
+          break;
+        case this.#itemTypes.sulfuras: 
+          item.quality = 80; // Sulfuras never alters
+          letSell = false;
+          break;
+        case this.#itemTypes.conjured:
+          item.quality = ShopHelper.decreaseInt(item.quality, 2, 0);
+          break;
+        default:
+          item.quality = ShopHelper.decreaseInt(item.quality, item.sellIn < 0 ? 2 : 1 , 0);
+          break;
+      }
+
+      if(letSell){
+        item.sellIn = ShopHelper.decreaseInt(item.sellIn, 1);
+      } //? else "does sellIn should reset to 0"???
+
+    });
     return this.items;
+  }
+}
+
+const ShopHelper = {
+
+  increaseInt : (num, by=1, max=50) => {
+    let result = by;
+    if(Number.isInteger(num)){
+      result = num + by;
+      result = result <= max ? result : max;
+    }
+    return result;
+  },
+
+  decreaseInt : (num, by=1, min=undefined) => {
+    let result = 0;
+    if(Number.isInteger(num)){
+      result = num - by;
+      if (min !== undefined){
+        result = result < min ? min : result;
+      };
+    }
+    return result;
   }
 }
 module.exports = {
   Item,
-  Shop
+  Shop,
+  ShopHelper
 }
